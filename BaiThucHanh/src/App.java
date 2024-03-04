c51aad89103fb7a6dc2e518919dced63293084e1
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2024-03-04 17:54:25 +0700

    BaiThucHanh 28/02

--- BaiThucHanh/src/App.java
+++ BaiThucHanh/src/App.java
@@ -1,5 +1,9 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+       Employee nv = new Employee();
+       nv.nhap();
+       nv.tinhTuoi();
+       nv.tinhLuong();
+       nv.in();    
     }
 }
